a7248ea1cf26eb311c32143410d4ac99f49bd004
IVxVI <ivan665303@gmail.com>
2022-06-19 20:32:34 +0000

    Comment

---
+++ js/index.js
@@ -0,0 +1,27 @@
+//set initial count
+let count = 0;
+
+//seleect value and the buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function(e){
+        const styles = e.currentTarget.classList;           //тут ми задали вибір для об'єкта(кнопки) на яку натискаємо
+        if(styles.contains("decrease")) {
+            count--;
+        } else if(styles.contains('increase')){
+            count++;
+        } else{
+            count = 0;
+        }
+        if(count > 0){
+            value.style.color = 'green'
+        } else if(count < 0){
+            value.style.color = 'red'
+        } else {
+            value.style.color = '#222'
+        }
+        value.textContent = count;
+    });
+});
